9a9d2e3c4a63e17dc0d8643cfdbf7638810c7bbc
Artifact <artifact911@gmail.com>
2021-10-12 10:51:55 +0300

    Add Logger

---
+++ src/main/java/org/example/core/logger/Slf4jLogbackExample.java
@@ -0,0 +1,35 @@
+package org.example.core.logger;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Подключены зависимости в POM.xml
+ * Добавлен файл в logback.xml в resources
+ */
+public class Slf4jLogbackExample {
+
+    private static final Logger logger = LoggerFactory.getLogger(Slf4jLogbackExample.class);
+
+    private static final String FILENAME = "/file/does/not/exist";
+
+
+    public static void main(String[] args) {
+
+        logger.info("Just a log message");
+        logger.debug("Message for debug level.");
+
+        try {
+            Files.readAllBytes(Paths.get(FILENAME));
+        } catch (IOException e) {
+            logger.error("Failed to read file {}", FILENAME, e);
+        }
+    }
+
+
+}
